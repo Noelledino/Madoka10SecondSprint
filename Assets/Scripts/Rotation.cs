1297b12f5901f7265e9735937cb3e74dc5180bfb
Noelle <noelledino@gmail.com>
2017-05-08 07:27:44 -0400

    Background Update: Doily Crochets from Dream Sequence

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	public float speed = 7f;
+
+	void Update ()
+	{
+		transform.Rotate (0,0,speed*Time.deltaTime); //rotates around z axis
+	}
+}
