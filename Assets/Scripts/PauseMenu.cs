d4f44d5d856da8f4d38b433bb0f5cf7f2ebc28d0
Unknown <noelledino@gmail.com>
2017-05-08 01:36:40 -0400

    Added Pause Menu
    Pause Menu functions and works well

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -7,7 +7,28 @@ public class PauseMenu : MonoBehaviour {
 	public string mainMenu;
 	public string restart;
 
+	public bool isPaused;
+
+	public GameObject pauseMenuCanvas;
+
+	void Update () {
+		if (isPaused) {
+			pauseMenuCanvas.SetActive (true);
+			Time.timeScale = 0f;
+		} else {
+			pauseMenuCanvas.SetActive (false);
+			Time.timeScale = 1f;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			isPaused = !isPaused;
+		}
+
+	}
+
+
 	public void unPause(){
+		isPaused = false;
 	}
 
 	public void RestartGame(){
